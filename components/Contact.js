a3ea7cd43fb1a7c069713f6c38ca14b2276cddd6
Katherina Curley <paytonagreen@gmail.com>
2020-09-24 00:03:16 -0500

    fixed imports

--- components/Contact.js
+++ components/Contact.js
@@ -1,9 +1,6 @@
 import React from "react";
 import Head from 'next/head';
 import ContactStyles from "./styles/ContactStyles";
-import github from "../public/img/PNG/actualgithub.png";
-import linkedin from "../public/img/PNG/linkedin.png";
-import email from "../public/img/PNG/email.png";
 
 
 const Contact = () => {
@@ -15,15 +12,7 @@ const Contact = () => {
       </title>
     </Head>
     <ContactStyles>
-      <div className="contact">
-        <img src={email} alt="" /> <p>Write Me!</p>
-      </div>
-      <div className="contact">
-        <img src={github} alt="" /> <p>Code With Me!</p>
-      </div>
-      <div className="contact">
-        <img src={linkedin} alt="" /> <p>Hire Me!</p>
-      </div>
+      
     </ContactStyles>
     </>
   );
