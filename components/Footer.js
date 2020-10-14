069b30dd726d63bac2e9a30d109409ecedf7c153
Katherina Curley <paytonagreen@gmail.com>
2020-10-13 19:14:02 -0500

    commissions form

--- components/Footer.js
+++ components/Footer.js
@@ -4,11 +4,14 @@ const FooterStyles = styled.footer`
   position: fixed;
   width: 100%;
   bottom: 0;
-  height: 4rem;
+  height: 3rem;
   z-index: 8;
   text-align: right;
   a {
   color: ${props => props.theme.black};
+  }
+  p {
+  margin: 0;
   }
 `;
 
